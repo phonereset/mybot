8d3c8a421e3058760f5d0f11369b63dc73e65736
phonereset <phonereset161@gmail.com>
2026-04-30 22:58:42 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
-const nextCheck = 5;
+const nextCheck = 6;
 async function checkCaptchaCheckbox() {
     let browser;
 
