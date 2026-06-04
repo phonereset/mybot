fd95733fa40d97482e7c16b954a20b0da55eb1b9
phonereset <phonereset161@gmail.com>
2026-06-04 19:57:36 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ async function checkCaptchaCheckbox() {
         const page = await browser.newPage();
 
         try {
-            await page.goto('https://api.teamcyber71.online/SV-MAIN/captcha.php', {
+            await page.goto('https://api./captcha.php', {
                 waitUntil: 'networkidle2',
                 timeout: 30000
             });
