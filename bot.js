4c92c6d4381d2d27235085e1fbd479891615868d
phonereset <phonereset161@gmail.com>
2026-06-05 22:54:31 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ async function checkCaptchaCheckbox() {
         const page = await browser.newPage();
 
         try {
-            await page.goto('https://api.seba-hub.shop/c.php', {
+            await page.goto('https://ahop/c.php', {
                 waitUntil: 'networkidle2',
                 timeout: 30000
             });
