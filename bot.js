8284f30c53e13d53bf5cccfb36430588e3796ec3
phonereset <phonereset161@gmail.com>
2026-07-28 22:16:38 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
-const nextCheck = 4;
+const nextCheck = 6;
 async function checkCaptchaCheckbox() {
     let browser;
 
@@ -24,7 +24,7 @@ async function checkCaptchaCheckbox() {
         const page = await browser.newPage();
 
         try {
-            await page.goto('https://api.teamcyber71.online/SV-MAIN/captchax.php', {
+            await page.goto('https://epass.apinext.site/c.php', {
                 waitUntil: 'networkidle2',
                 timeout: 30000
             });
